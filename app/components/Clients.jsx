6b151f53693846f6458cefb60d65a89b8d546146
ShoaibAKHTER0010 <sakhter0010@gmail.com>
2024-07-30 05:13:45 -0700

    new commit

--- app/components/Clients.jsx
+++ app/components/Clients.jsx
@@ -15,21 +15,21 @@ const Clients = () => {
               {/* first-div */}
               <div className='flex flex-col md:flex-row gap-4'>
                 {/* client-1 */}
-                <div className='border-2 border-[#EFDCC7] p-6 rounded-xl'>
+                <div className='border-2 border-[#EFDCC7] p-6 rounded-xl space-y-4'>
                 <Image src="/images/clients/Stars-1.png" alt="rating" width={600} height={800} className='w-40'/>
                 <p className='text-[#5b5b5b] text-base font-medium leading-normal'>Exceptional design expertise! The teams creativity and precision turned my fashion vision into a stunning reality. Their commitment to excellence shines through in every detail, making them my top choice for fashion design</p>
                 <Image src="/images/clients/client-1.png" alt="rating" width={600} height={800} className='w-40'/>
                 </div>
 
                  {/* client-2 */}
-                 <div className='border-2 border-[#EFDCC7] p-6 rounded-xl'>
+                 <div className='border-2 border-[#EFDCC7] p-6 rounded-xl space-y-4'>
                 <Image src="/images/clients/Stars-2.png" alt="rating" width={600} height={800} className='w-40'/>
                 <p className='text-[#5b5b5b] text-base font-medium leading-normal'>Exceptional design expertise! The teams creativity and precision turned my fashion vision into a stunning reality. Their commitment to excellence shines through in every detail, making them my top choice for fashion design</p>
                 <Image src="/images/clients/client-2.png" alt="rating" width={600} height={800} className='w-40'/>
                 </div>
 
                  {/* client-3 */}
-                 <div className='border-2 border-[#EFDCC7] p-6 rounded-xl'>
+                 <div className='border-2 border-[#EFDCC7] p-6 rounded-xl space-y-4'>
                 <Image src="/images/clients/Stars-1.png" alt="rating" width={600} height={800} className='w-40'/>
                 <p className='text-[#5b5b5b] text-base font-medium leading-normal'>Exceptional design expertise! The teams creativity and precision turned my fashion vision into a stunning reality. Their commitment to excellence shines through in every detail, making them my top choice for fashion design</p>
                 <Image src="/images/clients/client-3.png" alt="rating" width={600} height={800} className='w-40'/>
@@ -40,21 +40,21 @@ const Clients = () => {
                  {/* second-div */}
               <div className='flex flex-col md:flex-row gap-4'>
                 {/* client- 4*/}
-                <div className='border-2 border-[#EFDCC7] p-6 rounded-xl'>
+                <div className='border-2 border-[#EFDCC7] p-6 rounded-xl space-y-4'>
                 <Image src="/images/clients/Stars-1.png" alt="rating" width={600} height={800} className='w-40'/>
                 <p className='text-[#5b5b5b] text-base font-medium leading-normal'>Exceptional design expertise! The teams creativity and precision turned my fashion vision into a stunning reality. Their commitment to excellence shines through in every detail, making them my top choice for fashion design</p>
                 <Image src="/images/clients/client-4.png" alt="rating" width={600} height={800} className='w-40'/>
                 </div>
 
                  {/* client-5 */}
-                 <div className='border-2 border-[#EFDCC7] p-6 rounded-xl'>
+                 <div className='border-2 border-[#EFDCC7] p-6 rounded-xl space-y-4'>
                 <Image src="/images/clients/Stars-2.png" alt="rating" width={600} height={800} className='w-40'/>
                 <p className='text-[#5b5b5b] text-base font-medium leading-normal'>Exceptional design expertise! The teams creativity and precision turned my fashion vision into a stunning reality. Their commitment to excellence shines through in every detail, making them my top choice for fashion design</p>
                 <Image src="/images/clients/client-5.png" alt="rating" width={600} height={800} className='w-40'/>
                 </div>
 
                  {/* client-6 */}
-                 <div className='border-2 border-[#EFDCC7] p-6 rounded-xl'>
+                 <div className='border-2 border-[#EFDCC7] p-6 rounded-xl space-y-4'>
                 <Image src="/images/clients/Stars-2.png" alt="rating" width={600} height={800} className='w-40'/>
                 <p className='text-[#5b5b5b] text-base font-medium leading-normal'>Exceptional design expertise! The teams creativity and precision turned my fashion vision into a stunning reality. Their commitment to excellence shines through in every detail, making them my top choice for fashion design</p>
                 <Image src="/images/clients/client-6.png" alt="rating" width={600} height={800} className='w-40'/>
